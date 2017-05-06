524e922eb12f91dd85855d7688b0ba8bb5c013d6
Tim Saunders <tim@sauntimo.org>
2017-05-06 18:11:03 +0100

    Added Sass and Gulp, bootstrap with theme and began to work on home page

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+
+// styles task
+
+gulp.task( 'styles', function(){
+	gulp.src( ['public/sass/*.scss', 'public/sass/**/*.scss'] )
+	.pipe( sass().on( 'error', sass.logError ) )
+	.pipe( gulp.dest( './public/css/' ) );
+}); 
+
+// Watch task
+
+gulp.task( 'default', function(){
+	gulp.watch( ['public/sass/*.scss', 'public/sass/**/*.scss'], ['styles'] );
+});
